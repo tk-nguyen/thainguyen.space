15cf42ff43a0ef1e10ce5344c9dd72c88f912c68
Nguyen Thai <shiroemon279@gmail.com>
2024-08-23 20:52:21 +0700

    Added Content-Security-Policy header

---
+++ static/js/remark.js
@@ -0,0 +1,6 @@
+var remark_config = {
+    host: "https://comments.thainguyen.space",
+    site_id: "b2d3751e-6b89-42aa-a2c5-4df2e755d4f8",
+    theme: "dark"
+}
+!function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);
